b2ba165c44ff17a75d4ca0c9638d72c5b11e9913
jessica0522 <jessica040532@gmail.com>
2016-12-16 22:41:18 +1300

    fix some issues of step2,like the next button or changing catalogue

--- webapp/app/modules/selling/controllers/product-add-step2.controller.js
+++ webapp/app/modules/selling/controllers/product-add-step2.controller.js
@@ -16,14 +16,23 @@ angular.module('selling').controller('AddProductStep2', ['$log','$scope','$http'
 		}
 		
 		$scope.showDetail = function(){
+			$scope.updoStepCompleted(2);
 			$http.get('/rest/api/catalogues/' + $scope.selectedCatalogue.catalogId)
-			.then(function(response){
-				$scope.catalogueDetail.push(response.data[0]);
+				.then(function(response){					
+					$scope.catalogueDetail = [];
+					$scope.catalogueDetail.push(response.data[0]);						
+					$scope.stepCompleted(2);									
 			})
 		}
 
+		//check if the catalogue has been chosen
 		$scope.isCatalogue = function(selectedCategory){
 			return $scope.selectedCatalogue.title && $scope.selectedCatalogue.catalogId; 
+		}
+
+		// check if the next button should be enabled
+		$scope.enableNext = function(){
+			return !($scope.catalogueDetail && $scope.selectedCatalogue.catalogId);
 		}
 	}
 ])
