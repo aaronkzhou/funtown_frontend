7166998e255f32e98eef99bb502af1bee22a7960
Aaron <aaaron.chou@gmail.com>
2017-02-19 00:15:04 +1300

    Register:API change and disable field when getting data from api

--- webapp/app/modules/auth/controllers/register.controller.js
+++ webapp/app/modules/auth/controllers/register.controller.js
@@ -4,59 +4,79 @@ angular.module('auth').controller('RegisterController', ['$log','$scope', 'AuthS
 		$log.debug("RegisterController");
 		var new_res;
 		var address_selected;
+		var placeSearch, autocomplete,googleAPIaddress;
 		var params = {};
 		var city_postcode;
 		function init(){
 			$log.debug("Register::init");
-			$scope.register = {};
-			$scope.email = "";
-			$scope.username = "";
-			$scope.password = "";
-			$scope.confirm_password = "";
-			$scope.selectedAddress = "";
-			$scope.postcode = "";
-			$scope.surburb = "";
-			$scope.city = "";
-			$scope.address = "";
+			$scope.userRegister = {};
+			$scope.userRegister.email = "";
+			$scope.userRegister.username = "";
+			$scope.userRegister.password = "";
+			$scope.userRegister.confirm_password = "";
+			$scope.userRegister.selectedAddress = "";
+			$scope.userRegister.postcode = "";
+			$scope.userRegister.surburb = "";
+			$scope.userRegister.city = "";
+			$scope.userRegister.address = "";
+			$scope.userRegister.additionalAddress = "";
+			initAutocomplete();
 		}
 		$scope.authenticate = function(){				
 			$log.debug("register");
-			AuthService.getIfEmailExist($scope.email).then(function(){
-					$scope.email_exist = false;
+			AuthService.getIfEmailExist($scope.userRegister.email).then(function(){
+					$scope.userRegister.email_exist = false;
 				},function(err){
-					$scope.email_exist = true;
+					$scope.userRegister.email_exist = true;
 					//$scope.error = err.data.message;
 			});
-			AuthService.getIfUserNameExist($scope.username).then(function(){
-					$scope.username_exist = false;
+			AuthService.getIfUserNameExist($scope.userRegister.username).then(function(){
+					$scope.userRegister.username_exist = false;
 				},function(err){
-					$scope.username_exist = true;
+					$scope.userRegister.username_exist = true;
 					//$scope.error = err.data.message;
 			});
 
 		}
-		$scope.getAddress = function(viewValue) {
-		    params = {
-		    	address: viewValue, 
-		    	sensor: false,
-		    	language: 'en'
-		    };
-		    delete $http.defaults.headers.common["X-Requested-With"];
-		    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
-		    .then(function(res){
-		    	address_selected = res.data.results.filter(function(item){
-		    		return item.formatted_address.indexOf('New Zealand') !== -1;
-		    	});
-		    	return address_selected;
-		    });
-		};
-		$scope.populateAddress = function(){
-			new_res = $scope.selectedAddress.split(',');
-			city_postcode = new_res[2].trim().split(' ');
-			$scope.address = new_res[0].trim();
-			$scope.surburb = new_res[1].trim();
-			$scope.city = city_postcode[0].trim();
-			$scope.postcode = city_postcode[1].trim();
+		// $scope.getAddress = function(viewValue) {
+		//     params = {
+		//     	address: viewValue, 
+		//     	sensor: false,
+		//     	language: 'en'
+		//     };
+		//     delete $http.defaults.headers.common["X-Requested-With"];
+		//     return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
+		//     .then(function(res){
+		//     	address_selected = res.data.results.filter(function(item){
+		//     		return item.formatted_address.indexOf('New Zealand') !== -1;
+		//     	});
+		//     	return address_selected;
+		//     });
+		// };
+		function populateAddress(){
+			googleAPIaddress = autocomplete.getPlace().address_components;
+			console.log(googleAPIaddress);
+			$scope.userRegister.address = googleAPIaddress[0].long_name + ' ' + googleAPIaddress[1].long_name;
+			$scope.userRegister.surburb = googleAPIaddress[2].long_name;
+			$scope.userRegister.city = googleAPIaddress[4].long_name;
+			$scope.userRegister.postcode = googleAPIaddress[6].long_name;
+			$scope.userRegister.getFromGoogleApi = true;
+			$scope.$apply();
+		}
+
+		function initAutocomplete() {
+
+		  autocomplete = new google.maps.places.Autocomplete(
+		      /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+		      {
+		      	types: ['address'],
+		      	componentRestrictions: {country: 'NZ'},
+		      }
+		  );
+		  
+		  // When the user selects an address from the dropdown, populate the address
+		  // fields in the form.
+		  autocomplete.addListener('place_changed', populateAddress);
 		}
 
 		init();	
