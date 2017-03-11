cafc65556f172cfb9a10e15218fbcb36186d8606
CarolJCJC <jcjcmatsu@gmail.com>
2017-03-11 15:47:25 +1300

    register function 1) add the birthday field 2) modify the landline number 3) modify the mobile area number

--- webapp/app/modules/auth/controllers/register.controller.js
+++ webapp/app/modules/auth/controllers/register.controller.js
@@ -24,13 +24,18 @@ angular.module('auth').controller('RegisterController', ['$log','$scope', 'AuthS
 			$scope.userRegister.authenticated = false;
 			$scope.userRegister.genderOptions = "";
 			$scope.userRegister.LandlineAreaCode = getLandlineAreaCode();
+			$scope.userRegister.DateOfBirthDay = getDateOfBirthDay();
+			$scope.userRegister.DateOfBirthMonth = getDateOfBirthMonth();
 			$scope.userRegister.PhoneAreaCode = getPhoneAreaCode();
+			$scope.userRegister.BirthDay = "";
+			$scope.userRegister.BirthMonth = "";
+			$scope.userRegister.BirthYear = "";
 			$scope.userRegister.PhoneCode = "";
 			$scope.userRegister.LandlineCode = "";
 			$scope.userRegister.TnCCheckbox = false;
 			initAutocomplete();
 		}
-		$scope.authenticate = function(){				
+		$scope.authenticate = function(){
 			$log.debug("register:authenticate");
 			$scope.userRegister.authenticated = true;
 			AuthService.getIfEmailExist($scope.userRegister.email);
@@ -46,7 +51,7 @@ angular.module('auth').controller('RegisterController', ['$log','$scope', 'AuthS
 		}
 		// $scope.getAddress = function(viewValue) {
 		//     params = {
-		//     	address: viewValue, 
+		//     	address: viewValue,
 		//     	sensor: false,
 		//     	language: 'en'
 		//     };
@@ -78,18 +83,24 @@ angular.module('auth').controller('RegisterController', ['$log','$scope', 'AuthS
 		      	componentRestrictions: {country: 'NZ'},
 		      }
 		  );
-		  
+
 		  // When the user selects an address from the dropdown, populate the address
 		  // fields in the form.
 		  autocomplete.addListener('place_changed', populateAddress);
 		}
 		function getLandlineAreaCode(){
-			return ['1','2','3'];
+			return ['03','04','06','07','09','+61'];
+		}
+		function getDateOfBirthDay(){
+			return ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
+		}
+		function getDateOfBirthMonth(){
+			return ['January','Feburary','March','April','May','June','July','August','September','October','November','December'];
 		}
 		function getPhoneAreaCode(){
-			return ['4','5','6'];
+			return ['020','021','022','027','028','029'];
 		}
 
-		init();	
+		init();
 	}
 ])
